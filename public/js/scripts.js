45b7608aba520794968bc1a52b1c47d347207289
Mats Rörbecker <mats@rorbecker.com>
2015-12-07 21:34:08 +0100

    Adds autocomplete widget to form

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,11 @@
 $(function() {
+    $.getJSON('assets/municipalities.json', function(data) {
+        $('#municipality').autocomplete({
+            source: data
+        });
+    });
+
+
     $('#municipality').focus(function() {
         var field = $(this);
         if (field.val() == field.attr('value'))
